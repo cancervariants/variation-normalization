a487eca19932fbca44a58cf1e76e8c0d4876ba03
Adam Coffman <acoffman@wustl.edu>
2019-10-09 18:35:45 -0500

    prod build

---
+++ frontend/build/precache-manifest.9e3d37d30730fea18bc4652bfd2aac37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c29703c89828f77f2fb1ccfe7a6996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dd22420707d2ecd47a6",
+    "url": "/static/css/2.5a3c8dd4.chunk.css"
+  },
+  {
+    "revision": "631ed8857bc793675ab7",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4dd22420707d2ecd47a6",
+    "url": "/static/js/2.d7e0051d.chunk.js"
+  },
+  {
+    "revision": "631ed8857bc793675ab7",
+    "url": "/static/js/main.0a584b15.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  }
+]);
