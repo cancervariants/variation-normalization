fdae1cfd2e95d47bc6cef54d7e47c0f3d13229a9
Adam Coffman <acoffman@wustl.edu>
2019-11-06 18:40:54 -0600

    test harness for yaml fixtures

--- tests/tokenizers/test_fusion.py
+++ tests/tokenizers/test_fusion.py
@@ -1,31 +1,12 @@
-import unittest
-
 from varlexapp.tokenizers import Fusion
+from .tokenizer_test import TokenizerTest
 
-class TestFusionTokenizer(unittest.TestCase):
-    def setUp(self):
-        self.tokenizer = Fusion()
-        self.should_match = [
-            'Fusion',
-            'fusions',
-            'FUSION',
-            'fuSIons'
-        ]
+class TestFusionTokenizer(TokenizerTest):
+    def tokenizer_instance(self):
+        return Fusion()
 
-        self.should_not_match = [
-            'afusion',
-            'fused'
-        ]
+    def token_type(self):
+        return 'Fusion'
 
-    def test_matches(self):
-        for x in self.should_match:
-            res = self.tokenizer.match(x)
-            self.assertIsNotNone(res)
-            self.assertEqual('Fusion', res.token_type)
-
-        for x in self.should_not_match:
-            res = self.tokenizer.match(x)
-            self.assertIsNone(res)
-
-
-
+    def fixture_name(self):
+        return 'fusion'
