5fc2231fac57343c3c63a9bd5189a5de5be08739
korikuzma <korikuzma@gmail.com>
2021-02-04 16:54:00 -0500

    Add support for protein sequence

--- varlexapp/tokenizers/protein_substitution.py
+++ varlexapp/tokenizers/protein_substitution.py
@@ -11,11 +11,25 @@ class ProteinSubstitution(Tokenizer):
         self.splitter = re.compile(r'(\d+)')
 
     def match(self, input_string) -> Optional[ProteinSubstitutionToken]:
-        psub_parts = self.splitter.split(input_string)
+        if 'NP_' in input_string and ':p.' in input_string:
+            psub_parts = self.splitter.split(input_string.split(':')[-1])
+        else:
+            psub_parts = self.splitter.split(input_string)
+
+        result = self._get_psub(input_string, psub_parts)
+
+        if result:
+            return ProteinSubstitutionToken(input_string,
+                                            result[0],
+                                            result[1],
+                                            result[2])
+        return result
+
+    def _get_psub(self, input_string, psub_parts):
+        amino_acid = None
+        position = None,
+        new_amino_acid = None
         if len(psub_parts) == 3:
-            amino_acid = None
-            position = None,
-            new_amino_acid = None
             if self._is_valid_amino_acid(psub_parts[0], psub_parts[2]):
                 amino_acid = psub_parts[0]
                 position = psub_parts[1]
@@ -33,11 +47,8 @@ class ProteinSubstitution(Tokenizer):
                 if not self._is_valid_amino_acid(amino_acid, new_amino_acid):
                     return None
             if amino_acid and position and new_amino_acid:
-                return ProteinSubstitutionToken(input_string,
-                                                amino_acid,
-                                                new_amino_acid,
-                                                int(position))
-        return None
+                return amino_acid, new_amino_acid, int(position)
+
 
     def _is_valid_amino_acid(self, amino_acid, new_amino_acid):
         """Returns whether or not amino acids are valid."""
