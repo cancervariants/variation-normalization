e83475442bdcdf2a4c5b6e92b78a1487d34970d9
korikuzma <korikuzma@gmail.com>
2021-02-05 08:35:27 -0500

    Add docstrings

--- varlexapp/tokenizers/protein_substitution.py
+++ varlexapp/tokenizers/protein_substitution.py
@@ -1,3 +1,4 @@
+"""A module for Protein Substitution Tokenization."""
 import re
 from typing import Optional
 from .caches import AminoAcidCache
@@ -6,17 +7,28 @@ from ..models import ProteinSubstitutionToken
 
 
 class ProteinSubstitution(Tokenizer):
+    """Class for tokenizing Protein Substitution."""
+
     def __init__(self, amino_acid_cache: AminoAcidCache) -> None:
+        """Initialize the Protein Substitution Class.
+
+        :param AminoAcidCache amino_acid_cache: Valid amino acid codes.
+        """
         self.amino_acid_cache = amino_acid_cache
         self.splitter = re.compile(r'(\d+)')
 
     def match(self, input_string) -> Optional[ProteinSubstitutionToken]:
-        if 'NP_' in input_string and ':p.' in input_string:
+        """Return a ProteinSubstitutionToken match if one exists.
+
+        :param str input_string: The input string to match
+        :return: A ProteinSubstitutionToken if a match exists. Otherwise, None.
+        """
+        if ':p.' in input_string:
             psub_parts = self.splitter.split(input_string.split(':')[-1])
         else:
             psub_parts = self.splitter.split(input_string)
 
-        result = self._get_psub(input_string, psub_parts)
+        result = self._get_psub(psub_parts)
 
         if result:
             return ProteinSubstitutionToken(input_string,
@@ -25,7 +37,13 @@ class ProteinSubstitution(Tokenizer):
                                             result[2])
         return result
 
-    def _get_psub(self, input_string, psub_parts):
+    def _get_psub(self, psub_parts):
+        """Return Protein Substitution tokens.
+
+        :param list psub_parts: The split input string
+        :return: A tuple containing the amino acid, position, and new amino
+            acid
+        """
         amino_acid = None
         position = None,
         new_amino_acid = None
@@ -49,9 +67,11 @@ class ProteinSubstitution(Tokenizer):
             if amino_acid and position and new_amino_acid:
                 return amino_acid, new_amino_acid, int(position)
 
-
     def _is_valid_amino_acid(self, amino_acid, new_amino_acid):
-        """Returns whether or not amino acids are valid."""
+        """Return whether or not amino acids are valid.
+
+        :param str amino_acid: Reference amino acid
+        :param str new_amino_acid: New amino acid
+        """
         return (amino_acid in self.amino_acid_cache and
                 new_amino_acid in self.amino_acid_cache)
-
