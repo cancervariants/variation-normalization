b4b40e01794c920f62bed36b40aa93508ad86079
korikuzma <korikuzma@gmail.com>
2021-02-05 09:22:56 -0500

    Add more docstrings. Use constants for amino acid cache

--- tests/tokenizers/test_protein_substitution.py
+++ tests/tokenizers/test_protein_substitution.py
@@ -1,15 +1,21 @@
+"""A module for testing Protein Substitution tokenization."""
 import unittest
-
 from varlexapp.tokenizers import ProteinSubstitution
 from .tokenizer_base import TokenizerBase
 from varlexapp.tokenizers.caches import AminoAcidCache
 
+
 class TestProteinSubstitutionTokenizer(TokenizerBase, unittest.TestCase):
+    """A class for testing Protein Substitution Tokenization."""
+
     def tokenizer_instance(self):
+        """Return protein substitution instance."""
         return ProteinSubstitution(AminoAcidCache())
 
     def token_type(self):
+        """Return protein substitution token type."""
         return 'ProteinSubstitution'
 
     def fixture_name(self):
+        """Return the fixture name for protein substitution."""
         return 'protein_substitution'
