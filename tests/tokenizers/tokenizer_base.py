7091bd2de8dfd619aa3607f74b93bcac42f91cdd
korikuzma <korikuzma@gmail.com>
2021-02-05 09:46:59 -0500

    Clean up protein substitution tokenizer

--- tests/tokenizers/tokenizer_base.py
+++ tests/tokenizers/tokenizer_base.py
@@ -1,10 +1,13 @@
-import unittest
+"""A module for testing tokenizer classes."""
 import yaml
 from varlexapp import PROJECT_ROOT
 
+
 class TokenizerBase(object):
+    """The tokenizer base class."""
 
     def setUp(self):
+        """Set up the test test cases."""
         with open(f'{PROJECT_ROOT}/tests/fixtures/tokenizers.yml') as stream:
             self.all_fixtures = yaml.safe_load(stream)
         self.fixtures = self.all_fixtures.get(
@@ -14,21 +17,26 @@ class TokenizerBase(object):
         self.tokenizer = self.tokenizer_instance()
 
     def tokenizer_instance(self):
+        """Check that tokenizer_instance method is implemented."""
         raise NotImplementedError()
 
     def token_type(self):
+        """Check that token_type method is implemented."""
         raise NotImplementedError()
 
     def fixture_name(self):
+        """Check that fixture_name method is implemented."""
         raise NotImplementedError()
 
     def test_matches(self):
+        """Test that tokenizer matches correctly."""
         for x in self.fixtures['should_match']:
             res = self.tokenizer.match(x['token'])
             self.assertIsNotNone(res)
             self.assertEqual(self.token_type(), res.token_type)
 
     def test_not_matches(self):
+        """Test that tokenizer matches correctly."""
         for x in self.fixtures['should_not_match']:
             res = self.tokenizer.match(x['token'])
             self.assertIsNone(res)
--- varlexapp/tokenizers/protein_substitution.py
+++ varlexapp/tokenizers/protein_substitution.py
@@ -16,6 +16,11 @@ class ProteinSubstitution(Tokenizer):
         """
         self.amino_acid_cache = amino_acid_cache
         self.splitter = re.compile(r'(\d+)')
+        self.psub = {
+            'amino_acid': None,
+            'position': None,
+            'new_amino_acid': None
+        }
 
     def match(self, input_string) -> Optional[ProteinSubstitutionToken]:
         """Return a ProteinSubstitutionToken match if one exists.
@@ -23,51 +28,52 @@ class ProteinSubstitution(Tokenizer):
         :param str input_string: The input string to match
         :return: A ProteinSubstitutionToken if a match exists. Otherwise, None.
         """
+        if input_string is None:
+            return None
+
         if ':p.' in input_string:
             psub_parts = self.splitter.split(input_string.split(':')[-1])
         else:
             psub_parts = self.splitter.split(input_string)
 
-        result = self._get_psub(psub_parts)
+        self._get_psub(psub_parts)
 
-        if result:
+        if all(self.psub.values()):
+            if not self._is_valid_amino_acid(self.psub['amino_acid'],
+                                             self.psub['new_amino_acid']):
+                return None
             return ProteinSubstitutionToken(input_string,
-                                            result[0],
-                                            result[1],
-                                            result[2])
-        return result
+                                            self.psub['amino_acid'],
+                                            self.psub['new_amino_acid'],
+                                            self.psub['position'])
+        return None
 
     def _get_psub(self, psub_parts):
         """Return Protein Substitution tokens.
 
         :param list psub_parts: The split input string
-        :return: A tuple containing the amino acid, position, and new amino
-            acid
         """
-        amino_acid = None
-        position = None,
-        new_amino_acid = None
-
         # missense
         if len(psub_parts) == 3:
             if self._is_valid_amino_acid(psub_parts[0], psub_parts[2]):
-                amino_acid = psub_parts[0]
-                position = psub_parts[1]
-                new_amino_acid = psub_parts[2]
+                self._set_psub(psub_parts[0], psub_parts[1], psub_parts[2])
             elif 'p.' in psub_parts[0]:
                 # Predicted consequences given in parentheses
                 if '(' in psub_parts[0] and ')' in psub_parts[-1]:
-                    amino_acid = psub_parts[0].split('p.')[-1].split('(')[-1]
-                    position = psub_parts[1]
-                    new_amino_acid = psub_parts[-1].split(')')[0]
+                    self._set_psub(
+                        psub_parts[0].split('p.')[-1].split('(')[-1],
+                        psub_parts[1],
+                        psub_parts[-1].split(')')[0]
+                    )
                 else:
-                    amino_acid = psub_parts[0].split('p.')[-1]
-                    position = psub_parts[1]
-                    new_amino_acid = psub_parts[-1]
-                if not self._is_valid_amino_acid(amino_acid, new_amino_acid):
-                    return None
-            if amino_acid and position and new_amino_acid:
-                return amino_acid, new_amino_acid, int(position)
+                    self._set_psub(psub_parts[0].split('p.')[-1],
+                                   psub_parts[1],
+                                   psub_parts[-1])
+
+    def _set_psub(self, amino_acid, position, new_amino_acid):
+        self.psub['amino_acid'] = amino_acid
+        self.psub['position'] = position
+        self.psub['new_amino_acid'] = new_amino_acid
 
     def _is_valid_amino_acid(self, amino_acid, new_amino_acid):
         """Return whether or not amino acids are valid.
