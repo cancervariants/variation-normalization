0ea0b9e592974646104c3e81611dc7c1148947c6
korikuzma <korikuzma@gmail.com>
2021-02-10 13:06:58 -0500

    Add translator test

--- tests/translators/test_protein_substitution.py
+++ tests/translators/test_protein_substitution.py
@@ -1,11 +1,26 @@
 """Module for testing Protein Substitution Translator."""
 import unittest
+from varlexapp.classifiers import ProteinSubstitutionClassifier
 from varlexapp.translators import ProteinSubstitution
+from varlexapp.validators import ProteinSubstitution as PSUB_V
 from .translator_base import TranslatorBase
+from varlexapp.data_sources import SeqRepoAccess, TranscriptMappings
+from varlexapp import PROJECT_ROOT
 
 
 class TestProteinSubstitutionTranslator(TranslatorBase, unittest.TestCase):
     """A class to test the Protein Substitution Translator."""
+
+    def classifier_instance(self):
+        """Return protein substitution instance."""
+        return ProteinSubstitutionClassifier()
+
+    def validator_instance(self):
+        """Return protein substitution instance."""
+        return PSUB_V(SeqRepoAccess(
+            f"{PROJECT_ROOT}/varlexapp/data/seqrepo/latest"),
+            TranscriptMappings(f"{PROJECT_ROOT}/varlexapp/data"
+                               f"/transcript_mapping.tsv"))
 
     def translator_instance(self):
         """Return protein substitution instance."""
