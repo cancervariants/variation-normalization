9819ee8e2d52646e4aa65b3f1b71f359e6472b70
korikuzma <korikuzma@gmail.com>
2021-02-15 16:25:22 -0500

    Update schema examples

--- varlexapp/schemas/validation_response_schema.py
+++ varlexapp/schemas/validation_response_schema.py
@@ -1,19 +1,19 @@
 """Module for Validation Response Schema."""
 from pydantic import BaseModel
 from pydantic.types import StrictBool
-from typing import List, Optional
+from typing import List, Optional, Dict, Any, Type
 from enum import IntEnum
 from varlexapp.schemas.classification_response_schema import Classification
 
 
 class LookupType(IntEnum):
-    """Define constraints for Lookup Type."""
+    """IntEnum for Lookup Type."""
 
     GENE_SYMBOL = 1
 
 
 class ValidationResult(BaseModel):
-    """Define model for validation result."""
+    """Validation Results for a given variant."""
 
     classification: Classification
     is_valid: StrictBool
@@ -28,9 +28,71 @@ class ValidationResult(BaseModel):
 
         orm_mode = True
 
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['ValidationResult']) -> None:
+            """Configure OpenAPI schema."""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "classification": {
+                    "classification_type": 2,
+                    "matching_tokens": [
+                        "GeneSymbol",
+                        "ProteinSubstitution"
+                    ],
+                    "non_matching_tokens": [],
+                    "all_tokens": [
+                        {
+                            "token": "BRAF",
+                            "token_type": "GeneSymbol",
+                            "match_type": 2,
+                            "input_string": "BRAF",
+                            "object_type": "Token",
+                            "matched_value": "BRAF"
+                        },
+                        {
+                            "token": "V600E",
+                            "token_type": "ProteinSubstitution",
+                            "match_type": 5,
+                            "input_string": "V600E",
+                            "object_type": "Token",
+                            "ref_protein": "V",
+                            "alt_protein": "E",
+                            "position": 600
+                        }
+                    ],
+                    "confidence": 4
+                },
+                "is_valid": True,
+                "confidence_score": 1,
+                "allele": {
+                  "_id": "ga4gh:VA.u6sKlz0mMQvARmrlnt0Aksz6EbSkmL8z",
+                  "location": {
+                    "interval": {
+                      "end": 600,
+                      "start": 599,
+                      "type": "SimpleInterval"
+                    },
+                    "sequence_id": "ga4gh:SQ.ZJwurRo2HLY018wghYjDKSfIlEH0Y8At",
+                    "type": "SequenceLocation"
+                  },
+                  "state": {
+                    "sequence": "E",
+                    "type": "SequenceState"
+                  },
+                  "type": "Allele"
+                },
+                "human_description": "ENSP00000419060.2 V600E",
+                "concise_description": "ENSP00000419060.2 V600E",
+                "errors": []
+            }
+
 
 class ValidationSummary(BaseModel):
-    """Define model for validation summary."""
+    """Give Valid and Invalid Results for a given variant."""
 
     valid_results: List[ValidationResult]
     invalid_results: List[ValidationResult]
@@ -40,9 +102,132 @@ class ValidationSummary(BaseModel):
 
         orm_mode = True
 
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['ValidationSummary']) -> None:
+            """Configure OpenAPI schema."""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "valid_results": [
+                    {
+                        "classification": {
+                            "classification_type": 2,
+                            "matching_tokens": [
+                                "GeneSymbol",
+                                "ProteinSubstitution"
+                            ],
+                            "non_matching_tokens": [],
+                            "all_tokens": [
+                                {
+                                    "token": "BRAF",
+                                    "token_type": "GeneSymbol",
+                                    "match_type": 2,
+                                    "input_string": "BRAF",
+                                    "object_type": "Token",
+                                    "matched_value": "BRAF"
+                                },
+                                {
+                                    "token": "V600E",
+                                    "token_type": "ProteinSubstitution",
+                                    "match_type": 5,
+                                    "input_string": "V600E",
+                                    "object_type": "Token",
+                                    "ref_protein": "V",
+                                    "alt_protein": "E",
+                                    "position": 600
+                                }
+                            ],
+                            "confidence": 4
+                        },
+                        "is_valid": True,
+                        "confidence_score": 1,
+                        "allele": {
+                            "_id": "ga4gh:VA.u6sKlz0mMQvARmrlnt0Aksz6EbSkmL8z",
+                            "location": {
+                                "interval": {
+                                    "end": 600,
+                                    "start": 599,
+                                    "type": "SimpleInterval"
+                                },
+                                "sequence_id": "ga4gh:SQ.ZJwurRo2HLY018wghYjDKSfIlEH0Y8At",  # noqa: E501
+                                "type": "SequenceLocation"
+                            },
+                            "state": {
+                                "sequence": "E",
+                                "type": "SequenceState"
+                            },
+                            "type": "Allele"
+                        },
+                        "human_description": "ENSP00000419060.2 V600E",
+                        "concise_description": "ENSP00000419060.2 V600E",
+                        "errors": []
+                    }
+                ],
+                "invalid_results": [
+                    {
+                        "classification": {
+                            "classification_type": 2,
+                            "matching_tokens": [
+                                "GeneSymbol",
+                                "ProteinSubstitution"
+                            ],
+                            "non_matching_tokens": [],
+                            "all_tokens": [
+                                {
+                                    "token": "BRAF",
+                                    "token_type": "GeneSymbol",
+                                    "match_type": 2,
+                                    "input_string": "BRAF",
+                                    "object_type": "Token",
+                                    "matched_value": "BRAF"
+                                },
+                                {
+                                    "token": "V600E",
+                                    "token_type": "ProteinSubstitution",
+                                    "match_type": 5,
+                                    "input_string": "V600E",
+                                    "object_type": "Token",
+                                    "ref_protein": "V",
+                                    "alt_protein": "E",
+                                    "position": 600
+                                }
+                            ],
+                            "confidence": 4
+                        },
+                        "is_valid": False,
+                        "confidence_score": 1,
+                        "allele": {
+                            "_id": "ga4gh:VA.yj1Z7SReNgZx4m6nJ1rfJv3hXglXFwFc",
+                            "location": {
+                                "interval": {
+                                    "end": 600,
+                                    "start": 599,
+                                    "type": "SimpleInterval"
+                                },
+                                "sequence_id": "ga4gh:SQ.WaAJ_cXXn9YpMNfhcq9lnzIvaB9ALawo",  # noqa: E501
+                                "type": "SequenceLocation"
+                            },
+                            "state": {
+                                "sequence": "E",
+                                "type": "SequenceState"
+                            },
+                            "type": "Allele"
+                        },
+                        "human_description": "ENSP00000496776.1 V600E",
+                        "concise_description": "ENSP00000496776.1 V600E",
+                        "errors": [
+                            "Needed to find V at position 600 on ENSP00000496776.1 but found T"  # noqa: E501
+                        ]
+                    }
+                ]
+            }
+
 
 class ValidationResponseSchema(BaseModel):
-    """Define model for validation response."""
+    """Validation Response for a given variant."""
 
     search_term: str
     validation_summary: ValidationSummary
@@ -51,3 +236,127 @@ class ValidationResponseSchema(BaseModel):
         """Configure model."""
 
         orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['ValidationResponseSchema']) -> None:
+            """Configure OpenAPI schema."""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "search_term": "BRAF V600E",
+                "valid_results": [
+                    {
+                        "classification": {
+                            "classification_type": 2,
+                            "matching_tokens": [
+                                "GeneSymbol",
+                                "ProteinSubstitution"
+                            ],
+                            "non_matching_tokens": [],
+                            "all_tokens": [
+                                {
+                                    "token": "BRAF",
+                                    "token_type": "GeneSymbol",
+                                    "match_type": 2,
+                                    "input_string": "BRAF",
+                                    "object_type": "Token",
+                                    "matched_value": "BRAF"
+                                },
+                                {
+                                    "token": "V600E",
+                                    "token_type": "ProteinSubstitution",
+                                    "match_type": 5,
+                                    "input_string": "V600E",
+                                    "object_type": "Token",
+                                    "ref_protein": "V",
+                                    "alt_protein": "E",
+                                    "position": 600
+                                }
+                            ],
+                            "confidence": 4
+                        },
+                        "is_valid": True,
+                        "confidence_score": 1,
+                        "allele": {
+                            "_id": "ga4gh:VA.u6sKlz0mMQvARmrlnt0Aksz6EbSkmL8z",
+                            "location": {
+                                "interval": {
+                                    "end": 600,
+                                    "start": 599,
+                                    "type": "SimpleInterval"
+                                },
+                                "sequence_id": "ga4gh:SQ.ZJwurRo2HLY018wghYjDKSfIlEH0Y8At",  # noqa: E501
+                                "type": "SequenceLocation"
+                            },
+                            "state": {
+                                "sequence": "E",
+                                "type": "SequenceState"
+                            },
+                            "type": "Allele"
+                        },
+                        "human_description": "ENSP00000419060.2 V600E",
+                        "concise_description": "ENSP00000419060.2 V600E",
+                        "errors": []
+                    }
+                ],
+                "invalid_results": [
+                    {
+                        "classification": {
+                            "classification_type": 2,
+                            "matching_tokens": [
+                                "GeneSymbol",
+                                "ProteinSubstitution"
+                            ],
+                            "non_matching_tokens": [],
+                            "all_tokens": [
+                                {
+                                    "token": "BRAF",
+                                    "token_type": "GeneSymbol",
+                                    "match_type": 2,
+                                    "input_string": "BRAF",
+                                    "object_type": "Token",
+                                    "matched_value": "BRAF"
+                                },
+                                {
+                                    "token": "V600E",
+                                    "token_type": "ProteinSubstitution",
+                                    "match_type": 5,
+                                    "input_string": "V600E",
+                                    "object_type": "Token",
+                                    "ref_protein": "V",
+                                    "alt_protein": "E",
+                                    "position": 600
+                                }
+                            ],
+                            "confidence": 4
+                        },
+                        "is_valid": False,
+                        "confidence_score": 1,
+                        "allele": {
+                            "_id": "ga4gh:VA.yj1Z7SReNgZx4m6nJ1rfJv3hXglXFwFc",
+                            "location": {
+                                "interval": {
+                                    "end": 600,
+                                    "start": 599,
+                                    "type": "SimpleInterval"
+                                },
+                                "sequence_id": "ga4gh:SQ.WaAJ_cXXn9YpMNfhcq9lnzIvaB9ALawo",  # noqa: E501
+                                "type": "SequenceLocation"
+                            },
+                            "state": {
+                                "sequence": "E",
+                                "type": "SequenceState"
+                            },
+                            "type": "Allele"
+                        },
+                        "human_description": "ENSP00000496776.1 V600E",
+                        "concise_description": "ENSP00000496776.1 V600E",
+                        "errors": [
+                            "Needed to find V at position 600 on ENSP00000496776.1 but found T"  # noqa: E501
+                        ]
+                    }
+                ]
+            }
