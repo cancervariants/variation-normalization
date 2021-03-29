3b577258de3b84f6599fc660c91e1fd892671b58
korikuzma <korikuzma@gmail.com>
2021-03-29 14:00:30 -0400

    Update eb termination

--- codebuild/terminate_eb_env.py
+++ codebuild/terminate_eb_env.py
@@ -2,6 +2,7 @@
 import boto3
 import json
 import time
+
 client = boto3.client('lambda')
 servicecatalog = boto3.client('servicecatalog')
 eb_env_name = "VariantNormalization-staging-env"
@@ -19,8 +20,12 @@ eb_provisioned_product_status = \
     product_status['ProvisionedProductDetail']['Status']
 while eb_provisioned_product_status == "UNDER_CHANGE":
     time.sleep(10)
-    product_status = servicecatalog.describe_provisioned_product(
-        Id=eb_provisioned_product_Id)
-    eb_provisioned_product_status = \
-        product_status['ProvisionedProductDetail']['Status']
+    try:
+        product_status = \
+            servicecatalog.describe_provisioned_product(
+                Id=eb_provisioned_product_Id)
+        eb_provisioned_product_status = \
+            product_status['ProvisionedProductDetail']['Status']
+    except:  # noqa: E722
+        eb_provisioned_product_status = "PRODUCT NOT FOUND"
     print(eb_provisioned_product_status)
