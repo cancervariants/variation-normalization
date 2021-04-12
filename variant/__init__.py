29c6d473f6709f9bf0c6586567e213d59dc70294
korikuzma <korikuzma@gmail.com>
2021-04-12 19:59:10 -0400

    Download gene symbol files in app init

--- variant/__init__.py
+++ variant/__init__.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import os
 import logging
+from ftplib import FTP
 
 __version__ = "0.1.11"
 
@@ -21,8 +22,29 @@ if Path(LOG_FN).exists():
     logger.addHandler(fhandler)
 logger.setLevel(logging.DEBUG)
 
+
+def data_download(path, domain, dir, fn):
+    """Download files using FTP.
+
+    :param Path path: The path to the file
+    :param str domain: The domain of the FTP site
+    :param str dir: The directory that the file is located in
+    :param str fn: The file name to download
+    """
+    if not Path(path).exists():
+        with FTP(domain) as ftp:
+            ftp.login()
+            ftp.cwd(dir)
+            with open(path, 'wb') as fp:
+                ftp.retrbinary(f'RETR {fn}', fp.write)
+
+
 SEQREPO_DATA_PATH = f"{APP_ROOT}/data/seqrepo/latest"
 TRANSCRIPT_MAPPINGS_PATH = f"{APP_ROOT}/data/transcript_mapping.tsv"
 AMINO_ACID_PATH = f"{APP_ROOT}/data/amino_acids.csv"
 HGNC_GENE_SYMBOL_PATH = f"{APP_ROOT}/data/hgnc_gene_symbols.txt"
+data_download(HGNC_GENE_SYMBOL_PATH, 'ftp.ebi.ac.uk',
+              'pub/databases/genenames/new/tsv/', 'hgnc_complete_set.txt')
 REFSEQ_GENE_SYMBOL_PATH = f"{APP_ROOT}/data/refseq_gene_symbols.txt"
+data_download(REFSEQ_GENE_SYMBOL_PATH, 'ftp.ncbi.nih.gov',
+              'refseq/H_sapiens/RefSeqGene/', 'LRG_RefSeqGene')
