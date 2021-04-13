3265add76cfeaaf430e1213d645b4b8c0299cbac
korikuzma <korikuzma@gmail.com>
2021-04-13 15:37:38 -0400

    WIP: dna coding sub classifier

---
+++ variant/classifiers/coding_dna_substitution_classifier.py
@@ -0,0 +1,21 @@
+"""A module for the DNA Coding Substitution Classifier."""
+from typing import List
+from .set_based_classifier import SetBasedClassifier
+from variant.schemas.classification_response_schema import ClassificationType
+
+
+class DNACodingSubstitutionClassifier(SetBasedClassifier):
+    """The DNA Coding Substitution Classifier class."""
+
+    def classification_type(self) -> ClassificationType:
+        """Return the DNA Coding Substitution classification type."""
+        return ClassificationType.DNA_CODING_SUBSTITUTION
+
+    def exact_match_candidates(self) -> List[List[str]]:
+        """Return the exact match token type candidates."""
+        return [
+            ['CodingDNASubstitution'],
+            ['AminoAcidSubstitution', 'CodingDNASubstitution'],
+            ['HGVS', 'CodingDNASubstitution'],
+            ['ReferenceSequence', 'CodingDNASubstitution']
+        ]
