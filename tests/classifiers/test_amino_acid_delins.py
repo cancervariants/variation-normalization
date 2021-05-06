200156793b46f9e7c4b7e1141bd09a6d7340805f
korikuzma <korikuzma@gmail.com>
2021-05-06 14:05:44 -0400

    Add amino acid delins classifier

---
+++ tests/classifiers/test_amino_acid_delins.py
@@ -0,0 +1,16 @@
+"""Module for testing Amino Acid DelIns Classifier."""
+import unittest
+from variant.classifiers import AminoAcidDelInsClassifier
+from .classifier_base import ClassifierBase
+
+
+class TestAminoAcidDelInsClassifier(ClassifierBase, unittest.TestCase):
+    """A class to test the Amino Acid DelIns Classifier."""
+
+    def classifier_instance(self):
+        """Return AminoAcidDelInsClassifier instance."""
+        return AminoAcidDelInsClassifier()
+
+    def fixture_name(self):
+        """Return AminoAcidDelInsClassifier fixture name."""
+        return 'amino_acid_delins'
