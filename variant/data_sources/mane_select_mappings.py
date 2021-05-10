c162db22c3903a7510dc9731b1bb6837c24a14ee
korikuzma <korikuzma@gmail.com>
2021-05-10 15:38:25 -0400

    WIP: Mane transcript

---
+++ variant/data_sources/mane_select_mappings.py
@@ -0,0 +1,9 @@
+"""The module for MANE Select mappings."""
+
+
+class MANESelectMappings:
+    """The MANE Select mappings class."""
+
+    def __init__(self):
+        """Initialize the MANE Select mappings class."""
+        pass
