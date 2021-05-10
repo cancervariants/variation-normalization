885830a1cdc9fb73dfcee64173ed2ba0b328b69f
korikuzma <korikuzma@gmail.com>
2021-05-10 14:46:35 -0400

    Add refseq mane download

--- variant/__init__.py
+++ variant/__init__.py
@@ -1,16 +1,17 @@
 """The Variant Normalization package."""
 from pathlib import Path
-import os
 import logging
 from ftplib import FTP
-from os import environ
+from os import environ, remove
+import gzip
+import shutil
 
 __version__ = "0.2.0"
 
 APP_ROOT = Path(__file__).resolve().parents[0]
 environ['UTA_DB_URL'] = 'postgresql://anonymous@localhost:5432/uta/uta_20180821'  # noqa: E501
 
-if 'VARIANT_NORM_EB_PROD' in os.environ:
+if 'VARIANT_NORM_EB_PROD' in environ:
     # Elastic beanstalk
     LOG_FN = '/tmp/sample-app.log'
     environ['GENE_NORM_EB_PROD'] = "true"
@@ -38,8 +39,13 @@ def data_download(path, domain, dir, fn):
         with FTP(domain) as ftp:
             ftp.login()
             ftp.cwd(dir)
-            with open(path, 'wb') as fp:
+            with open(f"{path}.gz", 'wb') as fp:
                 ftp.retrbinary(f'RETR {fn}', fp.write)
+            if fn.endswith('.gz'):
+                with gzip.open(f"{path}.gz", 'rb') as f_in:
+                    with open(path, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                remove(f"{path}.gz")
 
 
 SEQREPO_DATA_PATH = f"{APP_ROOT}/data/seqrepo/latest"
@@ -51,3 +57,7 @@ data_download(HGNC_GENE_SYMBOL_PATH, 'ftp.ebi.ac.uk',
 REFSEQ_GENE_SYMBOL_PATH = f"{APP_ROOT}/data/refseq_gene_symbols.txt"
 data_download(REFSEQ_GENE_SYMBOL_PATH, 'ftp.ncbi.nih.gov',
               'refseq/H_sapiens/RefSeqGene/', 'LRG_RefSeqGene')
+REFSEQ_MANE_PATH = f"{APP_ROOT}/data/MANE.GRCh38.v0.93.summary.txt"
+data_download(REFSEQ_MANE_PATH, 'ftp.ncbi.nlm.nih.gov',
+              'refseq/MANE/MANE_human/release_0.93/',
+              'MANE.GRCh38.v0.93.summary.txt.gz')
