890fd59046f9f210f38a3b6bb58c14483cf529e2
korikuzma <korikuzma@gmail.com>
2021-05-13 16:03:32 -0400

    Clean up amino acid tokenization

--- variant/tokenizers/amino_acid_deletion.py
+++ variant/tokenizers/amino_acid_deletion.py
@@ -6,6 +6,7 @@ from .caches import AminoAcidCache
 from .tokenizer import Tokenizer
 from variant.schemas.token_response_schema import AminoAcidDeletionToken, \
     TokenMatchType
+from .amino_acid_base import AminoAcidBase
 
 
 class AminoAcidDeletion(Tokenizer):
@@ -16,15 +17,26 @@ class AminoAcidDeletion(Tokenizer):
 
         :param AminoAcidCache amino_acid_cache: Valid amino acid codes.
         """
-        self.amino_acid_cache = amino_acid_cache
         self.splitter = re.compile(r'del')
         self.splitter_char_digit = re.compile("([a-zA-Z]+)([0-9]+)")
         self.parts = None
+        self.amino_acid_base = AminoAcidBase(amino_acid_cache)
 
     def match(self, input_string: str) -> Optional[AminoAcidDeletionToken]:
         """Return token that match the input string."""
         if input_string is None:
             return None
+
+        self.parts = {
+            'used_one_letter': False,
+            'token': input_string,
+            'input_string': input_string,
+            'match_type': TokenMatchType.UNSPECIFIED.value,
+            'start_aa_del': None,
+            'start_pos_del': None,
+            'end_aa_del': None,
+            'end_pos_del': None
+        }
 
         input_string = str(input_string).lower()
 
@@ -39,17 +51,6 @@ class AminoAcidDeletion(Tokenizer):
 
         if not input_string.endswith('del'):
             return None
-
-        self.parts = {
-            'used_one_letter': False,
-            'token': input_string,
-            'input_string': input_string,
-            'match_type': TokenMatchType.UNSPECIFIED.value,
-            'start_aa_del': None,
-            'start_pos_del': None,
-            'end_aa_del': None,
-            'end_pos_del': None
-        }
 
         parts = self.splitter.split(input_string)
         self._get_parts(parts)
@@ -67,61 +68,10 @@ class AminoAcidDeletion(Tokenizer):
         if len(parts) != 2:
             return
 
-        # Get reference sequence
-        self._get_positions_deleted(parts)
-
-    def _get_positions_deleted(self, parts):
-        """Set position(s) deleted.
-
-        :param list parts: Tokenized input string
-        """
-        # Check positions deleted
-        if '_' in parts[0] and parts[0].count('_') == 1:
-            aa_pos_range = parts[0].split('_')
-            if len(aa_pos_range) != 2 or \
-                    not aa_pos_range[0] or not aa_pos_range[1]:
-                return None
-
-            start_aa_pos_del = \
-                self._get_amino_acid_and_pos(aa_pos_range[0])
-            end_aa_pos_del = \
-                self._get_amino_acid_and_pos(aa_pos_range[1])
-
-            if start_aa_pos_del and end_aa_pos_del:
-                self.parts['start_aa_del'] = start_aa_pos_del[0]
-                self.parts['start_pos_del'] = start_aa_pos_del[1]
-                self.parts['end_aa_del'] = end_aa_pos_del[0]
-                self.parts['end_pos_del'] = end_aa_pos_del[1]
-
-        else:
-            aa_and_pos = \
-                self._get_amino_acid_and_pos(parts[0])
-            if aa_and_pos:
-                self.parts['start_aa_del'] = aa_and_pos[0]
-                self.parts['start_pos_del'] = aa_and_pos[1]
-
-    def _get_amino_acid_and_pos(self, part):
-        """Return amino acid and position deleted.
-
-        :param list part: Tokenized input string
-        """
-        try:
-            char_and_digits = self.splitter_char_digit.match(part).groups()
-        except AttributeError:
-            return None
-
-        if len(char_and_digits) != 2 or len(part) != \
-                (len(char_and_digits[0]) + len(char_and_digits[1])):
-            return None
-
-        aa_del = char_and_digits[0]
-        if len(aa_del) == 1:
-            if not self.parts['used_one_letter']:
-                self.parts['used_one_letter'] = True
-            aa_del = self.amino_acid_cache.amino_acid_code_conversion[aa_del.upper()]  # noqa: E501
-        pos_del = char_and_digits[1]
-
-        if not self.amino_acid_cache.__contains__(aa_del) \
-                or not pos_del.isdigit():
-            return None
-        return aa_del.capitalize(), pos_del
+        range_aa_pos = self.amino_acid_base.get_possible_range(parts)
+        if range_aa_pos:
+            self.parts['start_aa_del'] = range_aa_pos[0]
+            self.parts['end_aa_del'] = range_aa_pos[1]
+            self.parts['start_pos_del'] = range_aa_pos[2]
+            self.parts['end_pos_del'] = range_aa_pos[3]
+            self.parts['used_one_letter'] = range_aa_pos[4]
--- variant/tokenizers/amino_acid_insertion.py
+++ variant/tokenizers/amino_acid_insertion.py
@@ -4,6 +4,7 @@ from typing import Optional
 from pydantic.error_wrappers import ValidationError
 from .caches import AminoAcidCache
 from .tokenizer import Tokenizer
+from .amino_acid_base import AminoAcidBase
 from variant.schemas.token_response_schema import AminoAcidInsertionToken, \
     TokenMatchType
 
@@ -16,10 +17,10 @@ class AminoAcidInsertion(Tokenizer):
 
         :param AminoAcidCache amino_acid_cache: Valid amino acid codes.
         """
-        self.amino_acid_cache = amino_acid_cache
         self.splitter = re.compile(r'ins')
         self.splitter_char_digit = re.compile("([a-zA-Z]+)([0-9]+)")
         self.parts = None
+        self.amino_acid_base = AminoAcidBase(amino_acid_cache)
 
     def match(self, input_string: str) -> Optional[AminoAcidInsertionToken]:
         """Return token that match the input string."""
@@ -67,84 +68,13 @@ class AminoAcidInsertion(Tokenizer):
         if len(parts) != 2:
             return
 
-        self._get_flanking_positions(parts)
-        self._get_inserted_sequence(parts)
-
-    def _get_flanking_positions(self, parts):
-        """Set flanking positions.
-
-        :param list parts: Tokenized input string
-        """
-        if '_' in parts[0] and parts[0].count('_') == 1:
-            aa_pos_range = parts[0].split('_')
-            if len(aa_pos_range) != 2 or \
-                    not aa_pos_range[0] or not aa_pos_range[1]:
-                return None
-
-            start_aa_pos_flank = \
-                self._get_amino_acid_and_pos(aa_pos_range[0])
-            end_aa_pos_flank = \
-                self._get_amino_acid_and_pos(aa_pos_range[1])
-
-            if start_aa_pos_flank and end_aa_pos_flank:
-                self.parts['start_aa_flank'] = start_aa_pos_flank[0]
-                self.parts['start_pos_flank'] = start_aa_pos_flank[1]
-                self.parts['end_aa_flank'] = end_aa_pos_flank[0]
-                self.parts['end_pos_flank'] = end_aa_pos_flank[1]
-
-    def _get_amino_acid_and_pos(self, part):
-        """Return amino acid and position flanked.
-
-        :param list part: Tokenized input string
-        """
-        try:
-            char_and_digits = self.splitter_char_digit.match(part).groups()
-        except AttributeError:
-            return None
-
-        if len(char_and_digits) != 2 or len(part) != \
-                (len(char_and_digits[0]) + len(char_and_digits[1])):
-            return None
-
-        aa = char_and_digits[0]
-        if len(aa) == 1:
-            if not self.parts['used_one_letter']:
-                self.parts['used_one_letter'] = True
-            aa = self.amino_acid_cache.amino_acid_code_conversion[aa.upper()]  # noqa: E501
-        pos_del = char_and_digits[1]
-
-        if not self.amino_acid_cache.__contains__(aa) \
-                or not pos_del.isdigit():
-            return None
-        return aa.capitalize(), pos_del
-
-    def _get_inserted_sequence(self, parts):
-        """Return inserted sequence.
-        :param list parts: Tokenized input string
-        """
-        # Check inserted sequences
-        inserted_sequence = ""
-        if self.parts['used_one_letter']:
-            for i in range(len(parts[1])):
-                aa = parts[1][i:i + 1]
-                if len(aa) != 1:
-                    return None
-                try:
-                    aa = self.amino_acid_cache.amino_acid_code_conversion[
-                        aa.upper()]  # noqa: E501
-                except KeyError:
-                    return None
-                else:
-                    inserted_sequence += aa
-
-        else:
-            for i in range(0, len(parts[1]), 3):
-                aa = parts[1][i:i + 3]
-                if len(aa) != 3 or not self.amino_acid_cache.__contains__(aa):
-                    if aa != 'ter':
-                        return None
-                inserted_sequence += aa.capitalize()
-
-        if inserted_sequence == '':
-            return None
-        self.parts['inserted_sequence'] = inserted_sequence
+        range_aa_pos = self.amino_acid_base.get_possible_range(parts)
+        if range_aa_pos:
+            self.parts['start_aa_flank'] = range_aa_pos[0]
+            self.parts['end_aa_flank'] = range_aa_pos[1]
+            self.parts['start_pos_flank'] = range_aa_pos[2]
+            self.parts['end_pos_flank'] = range_aa_pos[3]
+            self.parts['used_one_letter'] = range_aa_pos[4]
+        self.parts['inserted_sequence'] = \
+            self.amino_acid_base.get_inserted_sequence(
+                parts, self.parts['used_one_letter'])
