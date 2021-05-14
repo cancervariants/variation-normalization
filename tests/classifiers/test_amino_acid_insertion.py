4157002474857af523cfbbaf176c2e16e5d2c499
korikuzma <korikuzma@gmail.com>
2021-05-14 14:55:50 -0400

    Amino acid insertion classifier

---
+++ tests/classifiers/test_amino_acid_insertion.py
@@ -0,0 +1,16 @@
+"""Module for testing Amino Acid Insertion Classifier."""
+import unittest
+from variant.classifiers import AminoAcidInsertionClassifier
+from .classifier_base import ClassifierBase
+
+
+class TestAminoAcidInsertionClassifier(ClassifierBase, unittest.TestCase):
+    """A class to test the Amino Acid Insertion Classifier."""
+
+    def classifier_instance(self):
+        """Return AminoAcidInsertionClassifier instance."""
+        return AminoAcidInsertionClassifier()
+
+    def fixture_name(self):
+        """Return AminoAcidInsertionClassifier fixture name."""
+        return 'amino_acid_insertion'
