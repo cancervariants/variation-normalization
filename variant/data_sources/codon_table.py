7595037b94955b1e862c725b0ab078d2da6cf4df
korikuzma <korikuzma@gmail.com>
2021-05-28 14:45:35 -0400

    Change stop(-) to *

--- variant/data_sources/codon_table.py
+++ variant/data_sources/codon_table.py
@@ -35,8 +35,8 @@ class CodonTable:
             'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
             'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
             'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-            'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
-            'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
+            'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+            'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W',
         }
 
     def get_codons(self, amino_acid) -> List[str]:
