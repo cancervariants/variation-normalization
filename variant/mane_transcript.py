108099ab0a632869bf7362da6d7d6a56a8f5b7eb
korikuzma <korikuzma@gmail.com>
2021-05-28 16:14:18 -0400

    Only care about pos, ac

--- variant/mane_transcript.py
+++ variant/mane_transcript.py
@@ -1,9 +1,7 @@
 """Module for retrieving MANE transcript."""
-from typing import Optional, Tuple, List
-from variant.schemas.token_response_schema import ReferenceSequence, \
-    PolypeptideSequenceVariant
+from typing import Optional, Tuple
+from variant.schemas.token_response_schema import ReferenceSequence
 from variant.data_sources import CodonTable
-from variant.data_sources.codon_table import MULTIPLE_CODONS
 import hgvs.parser
 import logging
 
@@ -27,12 +25,12 @@ class MANETranscript:
         self.transcript_mappings = transcript_mappings
         self.codon_table = CodonTable(amino_acid_cache)
 
-    def p_to_c(self, transcript, token) -> Optional[str]:
+    def p_to_c(self, transcript, token) -> Optional[Tuple[str, int]]:
         """Convert protein (p.) to annotation to cDNA (c.) annotation.
 
         :param str transcript: Transcript accession
         :param Token token: Classification token
-        :return: HGVS expression at cDNA reference
+        :return: [cDNA transcript accession, cDNA position]
         """
         if token.reference_sequence != ReferenceSequence.PROTEIN:
             logger.debug(f"{token} does not have a "
@@ -47,87 +45,17 @@ class MANETranscript:
         else:
             return None
 
-        pos_mod_3 = token.position % 3
-        pos = token.position * 3
+        pos = self._p_to_c_pos(token.position)
+        return ac, pos
+
+    def _p_to_c_pos(self, p_pos) -> int:
+        """Return cDNA position given a protein position.
+
+        :param int p_pos: Protein position
+        :return: cDNA position
+        """
+        pos_mod_3 = p_pos % 3
+        pos = p_pos * 3
         if pos_mod_3 == 0:
             pos -= 1
-        elif pos_mod_3 == 1:
-            pos += 1
-
-        hgvs_c = f"{ac}:c.{pos}"
-        ref, alt = self._get_proteins(token)
-        if alt == '=':
-            alt = ref
-        ref_aa_codons, alt_aa_codons = self._get_codons(ref, alt)
-        change = self._c_change(ref_aa_codons, alt_aa_codons, ref, alt)
-
-        if change:
-            hgvs_c += change
-        else:
-            return None
-        return hgvs_c
-
-    def _get_proteins(self, token) -> Tuple[str, str]:
-        """Return reference and new amino acid for a given token.
-
-        :param Token token: Classification token
-        :return: [Reference amino acid, New amino acid]
-        """
-        ref = None
-        alt = None
-        if isinstance(token, PolypeptideSequenceVariant):
-            ref = token.ref_protein
-            alt = token.alt_protein
-        return ref, alt
-
-    def _get_codons(self, ref, alt) -> Tuple[List[str], List[str]]:
-        """Get codons for given reference and new amino acids.
-
-        :param str ref: Reference amino acid
-        :param str alt: New amino acid
-        :return: [Codons for reference amino acid, codons for new amino acid]
-        """
-        ref_aa_codons = self.codon_table.get_codons(ref)
-        alt_aa_codons = self.codon_table.get_codons(alt)
-        return ref_aa_codons, alt_aa_codons
-
-    def _get_nuc_index(self, ref_aa_codons, alt_aa_codons) -> Tuple[str, int]:
-        """Return nucleotide and index that match.
-
-        :param list ref_aa_codons: Codons for reference amino acid
-        :param list alt_aa_codons: Codons for new amino acid
-        :return: [Matching nucleotide, Index where nucleotides match]
-        """
-        i = 0
-        while i < 3:
-            for ref_codon in ref_aa_codons:
-                for alt_codon in alt_aa_codons:
-                    if ref_codon[i] == alt_codon[i]:
-                        return ref_codon[i], i
-            i += 1
-
-    def _c_change(self, ref_aa_codons, alt_aa_codons, ref, alt) -> str:
-        """Return variant at the cDNA reference.
-
-        :param list ref_aa_codons: Codons for reference amino acid
-        :param list alt_aa_codons: Codons for new amino acid
-        :param str ref: Reference amino acid
-        :param str alt: New amino acid
-        :return: Nucleotide change
-        """
-        if {ref, alt}.intersection(MULTIPLE_CODONS):
-            match, i = self._get_nuc_index(ref_aa_codons, alt_aa_codons)
-            ref_aa_codons = \
-                [codon for codon in ref_aa_codons if codon[i] == match]
-            alt_aa_codons = \
-                [codon for codon in alt_aa_codons if codon[i] == match]
-
-        i = 0
-        while i < 3:
-            for ref_codon in ref_aa_codons:
-                for alt_codon in alt_aa_codons:
-                    ref_nuc = ref_codon[i]
-                    alt_nuc = alt_codon[i]
-                    if ref_nuc != alt_nuc:
-                        return f"{ref_nuc}>{alt_nuc}"
-            i += 1
+        return pos
