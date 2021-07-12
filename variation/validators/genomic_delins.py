eb6de0f44bc401c0763b47991ed20cf86fce1eb7
korikuzma <korikuzma@gmail.com>
2021-07-12 11:22:09 -0400

    cleanup genomic delins validator

--- variation/validators/genomic_delins.py
+++ variation/validators/genomic_delins.py
@@ -5,7 +5,6 @@ from variation.schemas.classification_response_schema import \
 from variation.schemas.token_response_schema import GenomicDelInsToken
 from typing import List, Optional
 from variation.schemas.token_response_schema import GeneMatchToken
-from variation.schemas.token_response_schema import Token
 import logging
 
 
@@ -28,114 +27,60 @@ class GenomicDelIns(DelInsBase):
         """
         return self.get_genomic_transcripts(classification, errors)
 
-    def get_hgvs_expr(self, classification, t, s, is_hgvs) -> str:
-        """Return HGVS expression
-
-        :param Classification classification: A classification for a list of
-            tokens
-        :param str t: Transcript retrieved from transcript mapping
-        :param Token s: The classification token
-        :param bool is_hgvs: Whether or not classification is HGVS token
-        :return: hgvs expression
-        """
-        if not is_hgvs:
-            prefix = f"{t}:{s.reference_sequence.lower()}."
-            if s.start_pos_del is not None and s.end_pos_del is not None:
-                pos_del = f"{s.start_pos_del}_{s.end_pos_del}"
-            else:
-                pos_del = s.start_pos_del
-
-            if s.inserted_sequence1 is not None and \
-                    s.inserted_sequence2 is not None:
-                inserted_seq = f"{s.inserted_sequence1}_{s.inserted_sequence2}"
-            else:
-                inserted_seq = s.inserted_sequence1
-
-            hgvs_expr = f"{prefix}{pos_del}delins{inserted_seq}"
-        else:
-            hgvs_token = [t for t in classification.all_tokens if
-                          isinstance(t, Token) and t.token_type == 'HGVS'][0]
-            hgvs_expr = hgvs_token.input_string
-        return hgvs_expr
-
     def get_valid_invalid_results(self, classification_tokens, transcripts,
                                   classification, results, gene_tokens,
-                                  normalize_endpoint) -> None:
+                                  normalize_endpoint, mane_data_found,
+                                  is_identifier) -> None:
         """Add validation result objects to a list of results.
 
-        :param list classification_tokens: A list of Tokens
-        :param list transcripts: A list of transcript strings
+        :param list classification_tokens: A list of classification Tokens
+        :param list transcripts: A list of transcript accessions
         :param Classification classification: A classification for a list of
             tokens
-        :param list results: A list to store validation result objects
-        :param list gene_tokens: List of GeneMatchTokens
+        :param list results: Stores validation result objects
+        :param list gene_tokens: List of GeneMatchTokens for a classification
         :param bool normalize_endpoint: `True` if normalize endpoint is being
             used. `False` otherwise.
+        :param dict mane_data_found: MANE Transcript information found
+        :param bool is_identifier: `True` if identifier is given for exact
+            location. `False` otherwise.
         """
         valid_alleles = list()
-        if 'HGVS' in classification.matching_tokens:
-            is_hgvs = True
-        else:
-            is_hgvs = False
-
-        mane_data = {
-            'mane_select': dict(),
-            'mane_plus_clinical': dict(),
-            'longest_compatible_remaining': dict()
-        }
-
         for s in classification_tokens:
             for t in transcripts:
                 errors = list()
+                t = self.get_accession(t, classification)
+                allele = self.to_vrs_allele(t, s.start_pos_del, s.end_pos_del,
+                                            s.reference_sequence, s.alt_type,
+                                            errors, alt=s.inserted_sequence1)
 
-                allele, t, hgvs_expr, is_ensembl = \
-                    self.get_allele_with_context(classification, t, s, errors)
-
-                if allele:
+                if not errors:
                     self.check_pos_index(t, s, errors)
 
                 if not errors:
                     mane = self.mane_transcript.get_mane_transcript(
                         t, s.start_pos_del, s.end_pos_del,
                         s.reference_sequence,
+                        gene=gene_tokens[0].token if gene_tokens else None,
                         normalize_endpoint=normalize_endpoint
                     )
-                    if mane:
-                        if not gene_tokens and mane['gene']:
-                            gene_tokens.append(
-                                self._gene_matcher.match(mane['gene'])
-                            )
 
-                        if mane['status'] != 'GRCh38':
-                            s.molecule_context = 'transcript'
-                            s.reference_sequence = 'c'
-
-                        prefix = \
-                            f"{mane['refseq']}:{s.reference_sequence.lower()}."
-                        if s.start_pos_del is not None and s.end_pos_del is not None:  # noqa: E501
-                            pos_del = f"{mane['pos'][0]}_{mane['pos'][1]}"
-                        else:
-                            pos_del = f"{mane['pos'][0]}"
-                        if s.inserted_sequence1 is not None \
-                                and s.inserted_sequence2 is not None:
-                            inserted_seq = f"{s.inserted_sequence1}_" \
-                                           f"{s.inserted_sequence2}"
-                        else:
-                            inserted_seq = f"{s.inserted_sequence1}"
-                        mane_hgvs_expr = \
-                            f"{prefix}{pos_del}delins{inserted_seq}"
-                        self.add_mane_data(mane_hgvs_expr, mane, mane_data, s)
+                    self.add_mane_data(
+                        mane, mane_data_found, s.reference_sequence,
+                        s.alt_type, s, gene_tokens, alt=s.inserted_sequence1
+                    )
 
                 self.add_validation_result(
                     allele, valid_alleles, results,
                     classification, s, t, gene_tokens, errors
                 )
 
-                if is_hgvs:
+                if is_identifier:
                     break
 
         self.add_mane_to_validation_results(
-            mane_data, valid_alleles, results, classification, gene_tokens
+            mane_data_found, valid_alleles, results,
+            classification, gene_tokens
         )
 
     def get_gene_tokens(self, classification) -> List[GeneMatchToken]:
