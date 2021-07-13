f978904759c6a26bdc6897e02a89e468867597ca
korikuzma <korikuzma@gmail.com>
2021-07-13 15:51:04 -0400

    Add dp, tlr to validator/translator test params

--- tests/validators/test_polypeptide_truncation.py
+++ tests/validators/test_polypeptide_truncation.py
@@ -8,6 +8,8 @@ from variation.tokenizers.caches import GeneSymbolCache, AminoAcidCache
 from variation.data_sources import TranscriptMappings, SeqRepoAccess, \
     MANETranscriptMappings, UTA
 from variation.mane_transcript import MANETranscript
+from ga4gh.vrs.dataproxy import SeqRepoDataProxy
+from ga4gh.vrs.extras.translator import Translator
 
 
 class TestPolypeptideTruncationValidator(ValidatorBase, unittest.TestCase):
@@ -18,12 +20,13 @@ class TestPolypeptideTruncationValidator(ValidatorBase, unittest.TestCase):
         seqrepo_access = SeqRepoAccess()
         transcript_mappings = TranscriptMappings()
         uta = UTA()
+        dp = SeqRepoDataProxy(seqrepo_access.seq_repo_client)
+        tlr = Translator(data_proxy=dp)
         return PolypeptideTruncation(
             seqrepo_access, transcript_mappings, GeneSymbol(GeneSymbolCache()),
             MANETranscript(seqrepo_access, transcript_mappings,
                            MANETranscriptMappings(), uta),
-            uta,
-            AminoAcidCache())
+            uta, dp, tlr, AminoAcidCache())
 
     def classifier_instance(self):
         """Return the Polypeptide Truncation classifier instance."""
