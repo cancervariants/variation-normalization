b7d2f38cb318ce3f2c52c04c29b613174a7bfdc8
korikuzma <korikuzma@gmail.com>
2021-08-13 11:20:04 -0400

    use gene-normalizer location schemas, allele id is optional

--- variation/schemas/ga4gh_vrs.py
+++ variation/schemas/ga4gh_vrs.py
@@ -1,13 +1,15 @@
 """Module for modeling VRS objects."""
 from pydantic import BaseModel
 from pydantic.fields import Field
+from pydantic.types import StrictStr
 from typing import Dict, Any, Type, Optional
+from gene.schemas import SimpleInterval, SequenceLocation  # noqa: F401
 
 
 class SequenceState(BaseModel):
     """Captures a Sequence as a State."""
 
-    sequence: str
+    sequence: StrictStr
     type = 'SequenceState'
 
     class Config:
@@ -27,64 +29,10 @@ class SequenceState(BaseModel):
             }
 
 
-class SimpleInterval(BaseModel):
-    """A SequenceInterval with a single start and end coordinate."""
-
-    start: int
-    end: int
-    type = 'SimpleInterval'
-
-    class Config:
-        """Configure model."""
-
-        @staticmethod
-        def schema_extra(schema: Dict[str, Any],
-                         model: Type['SimpleInterval']) -> None:
-            """Configure OpenAPI schema."""
-            if 'title' in schema.keys():
-                schema.pop('title', None)
-            for prop in schema.get('properties', {}).values():
-                prop.pop('title', None)
-            schema['example'] = {
-                "end": 44908822,
-                "start": 44908821,
-                "type": "SimpleInterval"
-            }
-
-
-class SequenceLocation(BaseModel):
-    """A specified subsequence within another sequence that is used as a reference sequence."""  # noqa: E501
-
-    interval: SimpleInterval
-    sequence_id: str
-    type = 'SequenceLocation'
-
-    class Config:
-        """Configure model."""
-
-        @staticmethod
-        def schema_extra(schema: Dict[str, Any],
-                         model: Type['SequenceLocation']) -> None:
-            """Configure OpenAPI schema."""
-            if 'title' in schema.keys():
-                schema.pop('title', None)
-            for prop in schema.get('properties', {}).values():
-                prop.pop('title', None)
-            schema['example'] = {
-                "interval": {
-                    "end": 44908822,
-                    "start": 44908821,
-                    "type": "SimpleInterval"
-                },
-                "sequence_id": "ga4gh:SQ.IIB53T8CNeJJdUqzn9V_JnRtQadwWCbl",
-                "type": "SequenceLocation"
-            }
-
-
 class Allele(BaseModel):
     """A Sequence or Sequence change with respect to a reference sequence, without regard to genes or other features."""  # noqa: E501
 
-    id: str = Field(..., alias='_id')
+    id: Optional[StrictStr] = Field(alias='_id')
     location: SequenceLocation
     state: SequenceState
     type = 'Allele'
