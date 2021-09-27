9dc0cc0b040893b17bd9d5eb22e69c7e3244d43d
korikuzma <korikuzma@gmail.com>
2021-09-27 11:14:20 -0400

    Add hgvs_dup_del_mode to test_service_meta

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -1615,14 +1615,14 @@ def test_no_matches(test_normalize):
 
 def test_service_meta():
     """Test that service meta info populates correctly."""
-    response = normalize_get_response('BRAF v600e')
+    response = normalize_get_response('BRAF v600e', 'default')
     service_meta = response.service_meta_
     assert service_meta.name == "variation-normalizer"
     assert service_meta.version
     assert isinstance(service_meta.response_datetime, datetime)
     assert service_meta.url == 'https://github.com/cancervariants/variation-normalization'  # noqa: E501
 
-    response = normalize_get_response('this-wont-normalize')
+    response = normalize_get_response('this-wont-normalize', 'default')
     service_meta = response.service_meta_
     assert service_meta.name == "variation-normalizer"
     assert service_meta.version
