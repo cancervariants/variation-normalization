698ec905fd7e86f1ddc694fe6fffa756fbd71289
korikuzma <korikuzma@gmail.com>
2021-11-16 21:07:57 -0500

    Add gene normalizer to validator tests init

--- tests/validators/test_amino_acid_substitution.py
+++ tests/validators/test_amino_acid_substitution.py
@@ -23,12 +23,13 @@ class TestAminoAcidSubstitutionValidator(ValidatorBase, unittest.TestCase):
         uta = UTA()
         dp = SeqRepoDataProxy(seqrepo_access.seq_repo_client)
         tlr = Translator(data_proxy=dp)
+        gene_normalizer = GeneQueryHandler()
         return AminoAcidSubstitution(
             seqrepo_access, transcript_mappings,
-            GeneSymbol(GeneQueryHandler()),
+            GeneSymbol(gene_normalizer),
             MANETranscript(seqrepo_access, transcript_mappings,
                            MANETranscriptMappings(), uta),
-            uta, dp, tlr, AminoAcidCache())
+            uta, dp, tlr, gene_normalizer, AminoAcidCache())
 
     def classifier_instance(self):
         """Return the protein substitution classifier instance."""
