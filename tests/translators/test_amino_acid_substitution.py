363f9bb21585620c47714b2e37dcdc4208269c3e
korikuzma <korikuzma@gmail.com>
2021-11-16 20:30:27 -0500

    Forgot to add gene_normalizer to translator tests init

--- tests/translators/test_amino_acid_substitution.py
+++ tests/translators/test_amino_acid_substitution.py
@@ -28,12 +28,13 @@ class TestAminoAcidSubstitutionTranslator(TranslatorBase, unittest.TestCase):
         uta = UTA()
         dp = SeqRepoDataProxy(seqrepo_access.seq_repo_client)
         tlr = Translator(data_proxy=dp)
+        gene_normalizer = GeneQueryHandler()
         return AASUB_V(
             seqrepo_access, transcript_mappings,
-            GeneSymbol(GeneQueryHandler()),
+            GeneSymbol(gene_normalizer),
             MANETranscript(seqrepo_access, transcript_mappings,
                            MANETranscriptMappings(), uta),
-            uta, dp, tlr, AminoAcidCache())
+            uta, dp, tlr, gene_normalizer, AminoAcidCache())
 
     def translator_instance(self):
         """Return amino acid substitution instance."""
