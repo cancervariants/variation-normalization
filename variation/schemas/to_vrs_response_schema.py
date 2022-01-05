6c78bdb2e19f4dde1d4c32424e42e949cb33053a
Jiachen Liu <54122319+MayLiu27@users.noreply.github.com>
2022-01-05 11:56:38 -0500

    restore toVRS schema

--- variation/schemas/to_vrs_response_schema.py
+++ variation/schemas/to_vrs_response_schema.py
@@ -12,7 +12,6 @@ class ToVRSService(BaseModel):
 
     search_term: StrictStr
     warnings: Optional[List[StrictStr]]
-    # TODO: add possible_c_accessions filed to schema
     variations: Optional[Union[List[Allele], List[Text], List[Haplotype],
                                List[CopyNumber], List[VariationSet]]] = None
     service_meta_: ServiceMeta
@@ -52,8 +51,7 @@ class ToVRSService(BaseModel):
                         "state": {
                             "type": "LiteralSequenceExpression",
                             "sequence": "E"
-                        },
-                        "possible_c_accessions": ["NM_004333.6"]
+                        }
                     },
                     {
                         "_id": "ga4gh:VA.vimwyw0pFTwatfFhi3rhhb153ARWsPrW",
@@ -76,8 +74,7 @@ class ToVRSService(BaseModel):
                         "state": {
                             "type": "LiteralSequenceExpression",
                             "sequence": "E"
-                        },
-                        "possible_c_accessions": ["NM_001378474.1"]
+                        }
                     },
                     {
                         "_id": "ga4gh:VA.7ys8TiDzrk04O3Upd63__rOBCEhv3P5d",
@@ -94,8 +91,7 @@ class ToVRSService(BaseModel):
                         "state": {
                             "type": "LiteralSequenceExpression",
                             "sequence": "E"
-                        },
-                        "possible_c_accessions": ["NM_001354609.2"]
+                        }
                     },
                     {
                         "_id": "ga4gh:VA.FzlrH5feNcQ3S9GayMU9EF008j-8Pbz5",
@@ -112,8 +108,7 @@ class ToVRSService(BaseModel):
                         "state": {
                             "type": "LiteralSequenceExpression",
                             "sequence": "E"
-                        },
-                        "possible_c_accessions": ["NM_001378468.1"]
+                        }
                     },
                     {
                         "_id": "ga4gh:VA.8JkgnqIgYqufNl-OV_hpRG_aWF9UFQCE",
@@ -130,8 +125,7 @@ class ToVRSService(BaseModel):
                         "state": {
                             "type": "LiteralSequenceExpression",
                             "sequence": "E"
-                        },
-                        "possible_c_accessions": ["NM_001354609.2", "NM_001378474.1", "NM_004333.6", "NM_001378468.1"]
+                        }
                     }
                 ],
                 "service_meta_": {
