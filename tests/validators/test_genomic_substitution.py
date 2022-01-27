bb8bafec787fdea19605a636370e4108da99062e
korikuzma <korikuzma@gmail.com>
2022-01-26 21:03:25 -0500

    Fix validator/translator tests

--- tests/validators/test_genomic_substitution.py
+++ tests/validators/test_genomic_substitution.py
@@ -3,13 +3,6 @@ import unittest
 from variation.validators import GenomicSubstitution
 from variation.classifiers import GenomicSubstitutionClassifier
 from .validator_base import ValidatorBase
-from variation.tokenizers import GeneSymbol
-from variation.data_sources import TranscriptMappings, SeqRepoAccess, \
-    MANETranscriptMappings, UTA
-from variation.mane_transcript import MANETranscript
-from ga4gh.vrs.dataproxy import SeqRepoDataProxy
-from ga4gh.vrs.extras.translator import Translator
-from gene.query import QueryHandler as GeneQueryHandler
 
 
 class TestGenomicSubstitutionValidator(ValidatorBase, unittest.TestCase):
@@ -17,19 +10,7 @@ class TestGenomicSubstitutionValidator(ValidatorBase, unittest.TestCase):
 
     def validator_instance(self):
         """Return genomic substitution instance."""
-        seqrepo_access = SeqRepoAccess()
-        transcript_mappings = TranscriptMappings()
-        uta = UTA()
-        dp = SeqRepoDataProxy(seqrepo_access.seq_repo_client)
-        tlr = Translator(data_proxy=dp)
-        gene_normalizer = GeneQueryHandler()
-        return GenomicSubstitution(
-            seqrepo_access, transcript_mappings,
-            GeneSymbol(gene_normalizer),
-            MANETranscript(seqrepo_access, transcript_mappings,
-                           MANETranscriptMappings(), uta),
-            uta, dp, tlr, gene_normalizer
-        )
+        return GenomicSubstitution(*self.params)
 
     def classifier_instance(self):
         """Return the genomic substitution classifier instance."""
