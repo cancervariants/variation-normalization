4e9ff5fddf095eb45d4124775606d304279b9587
korikuzma <korikuzma@gmail.com>
2022-04-11 15:25:55 -0400

    Update dependencies (gene norm) + update tests

--- variation/schemas/normalize_response_schema.py
+++ variation/schemas/normalize_response_schema.py
@@ -109,8 +109,12 @@ class NormalizeService(ServiceResponse):
                             "ensembl:ENSG00000157764"
                         ],
                         "alternate_labels": [
-                            "B-Raf proto-oncogene, serine/threonine kinase",
-                            "BRAF1"
+                            "BRAF1",
+                            "RAFB1",
+                            "B-raf",
+                            "B-RAF1",
+                            "NS7",
+                            "BRAF-1"
                         ],
                         "extensions": [
                             {
@@ -119,21 +123,26 @@ class NormalizeService(ServiceResponse):
                                 "type": "Extension"
                             },
                             {
+                                "type": "Extension",
+                                "name": "approved_name",
+                                "value": "B-Raf proto-oncogene, serine/threonine kinase"
+                            },
+                            {
                                 "name": "associated_with",
                                 "value": [
-                                    "vega:OTTHUMG00000157457",
                                     "ucsc:uc003vwc.5",
-                                    "ccds:CCDS5863",
-                                    "ccds:CCDS87555",
-                                    "uniprot:P15056",
-                                    "pubmed:2284096",
                                     "pubmed:1565476",
-                                    "cosmic:BRAF",
                                     "omim:164757",
-                                    "orphanet:119066",
+                                    "vega:OTTHUMG00000157457",
+                                    "ccds:CCDS5863",
                                     "iuphar:1943",
+                                    "ccds:CCDS87555",
+                                    "orphanet:119066",
+                                    "refseq:NM_004333",
                                     "ena.embl:M95712",
-                                    "refseq:NM_004333"
+                                    "pubmed:2284096",
+                                    "uniprot:P15056",
+                                    "cosmic:BRAF"
                                 ],
                                 "type": "Extension"
                             },
@@ -151,6 +160,21 @@ class NormalizeService(ServiceResponse):
                                     }
                                 },
                                 "type": "Extension"
+                            },
+                            {
+                                "type": "Extension",
+                                "name": "hgnc_locus_type",
+                                "value": "gene with protein product"
+                            },
+                            {
+                                "type": "Extension",
+                                "name": "ncbi_gene_type",
+                                "value": "protein-coding"
+                            },
+                            {
+                                "type": "Extension",
+                                "name": "ensembl_biotype",
+                                "value": "protein_coding"
                             }
                         ]
                     }
