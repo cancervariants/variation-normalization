7ba09d650b2df59196d8c1e89d86b73fe1af1cab
korikuzma <korikuzma@gmail.com>
2022-08-16 13:00:07 -0400

    ops: change dev terminate_time to 30 days

--- codebuild/deploy_eb_env_dev.py
+++ codebuild/deploy_eb_env_dev.py
@@ -3,7 +3,7 @@ import boto3
 import time
 elasticbeanstalk = boto3.client('elasticbeanstalk')
 servicecatalog = boto3.client('servicecatalog')
-terminate_time = 12
+terminate_time = 720
 eb_app_name = "VariationNormalization"
 eb_env_name = "VariationNormalization-dev-env"
 sc_product_id = "prod-mmw6ymv2ntzl2"
