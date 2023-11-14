da230a5d3e0f44df1971589a42ff0a0dcb2e4937
Kori Kuzma <korikuzma@gmail.com>
2023-11-14 16:41:23 -0500

    feat: expose cool-seq-tool feature overlap endpoint (#523)
    For issue #521

--- variation/schemas/service_schema.py
+++ variation/schemas/service_schema.py
@@ -1,11 +1,18 @@
 """Module containing schemas for services"""
 from enum import Enum
-from typing import Any, Dict, Type
+from typing import Any, Dict, List, Optional, Type
 
-from cool_seq_tool.schemas import ToCdnaService as ToCdna
-from cool_seq_tool.schemas import ToGenomicService as ToGenomic
+from cool_seq_tool.schemas import (
+    CdsOverlap,
+)
+from cool_seq_tool.schemas import (
+    ToCdnaService as ToCdna,
+)
+from cool_seq_tool.schemas import (
+    ToGenomicService as ToGenomic,
+)
 
-from variation.schemas.normalize_response_schema import ServiceMeta
+from variation.schemas.normalize_response_schema import ServiceMeta, ServiceResponse
 
 
 class ClinVarAssembly(str, Enum):
@@ -75,6 +82,60 @@ class ToGenomicService(ToGenomic):
                     "residue_mode": "inter-residue",
                 },
                 "warnings": list(),
+                "service_meta": {
+                    "version": "0.5.4",
+                    "response_datetime": "2022-09-29T15:08:18.696882",
+                    "name": "variation-normalizer",
+                    "url": "https://github.com/cancervariants/variation-normalization",
+                },
+            }
+
+
+class FeatureOverlapService(ServiceResponse):
+    """Define model for representing Feature Overlap response"""
+
+    feature_overlap: Optional[Dict[str, List[CdsOverlap]]] = None
+
+    class Config:
+        """Configure model."""
+
+        @staticmethod
+        def schema_extra(
+            schema: Dict[str, Any], model: Type["FeatureOverlapService"]
+        ) -> None:
+            """Configure OpenAPI schema."""
+            if "title" in schema.keys():
+                schema.pop("title", None)
+            for prop in schema.get("properties", {}).values():
+                prop.pop("title", None)
+            schema["example"] = {
+                "feature_overlap": {
+                    "BRAF": [
+                        {
+                            "cds": {
+                                "_id": "ga4gh:VSL._H2ST69A4RkWCSRHOoMv-edt-R45fPdq",
+                                "type": "SequenceLocation",
+                                "sequence_id": "ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul",
+                                "interval": {
+                                    "type": "SequenceInterval",
+                                    "start": {"value": 140726493, "type": "Number"},
+                                    "end": {"value": 140726516, "type": "Number"},
+                                },
+                            },
+                            "overlap": {
+                                "_id": "ga4gh:VSL._H2ST69A4RkWCSRHOoMv-edt-R45fPdq",
+                                "type": "SequenceLocation",
+                                "sequence_id": "ga4gh:SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul",
+                                "interval": {
+                                    "type": "SequenceInterval",
+                                    "start": {"value": 140726493, "type": "Number"},
+                                    "end": {"value": 140726516, "type": "Number"},
+                                },
+                            },
+                        }
+                    ]
+                },
+                "warnings": [],
                 "service_meta": {
                     "version": "0.5.4",
                     "response_datetime": "2022-09-29T15:08:18.696882",
